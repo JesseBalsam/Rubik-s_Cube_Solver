17d34284d8d2b5007a49ae9483c491297b27a8f7
JesseBalsam <jsheabalsam@gmail.com>
2023-03-01 21:15:45 -0500

    Did more stuff and fixed stuff

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #Todo: Terminology for user, being able to upload a photo
 
 # frulbd
-cube = [['o', 'b', 'g', 'r', 'g', 'o', 'o', 'y', 'w'],
- ['o', 'b', 'w', 'b', 'r', 'y', 'r', 'w', 'b'],
- ['b', 'w', 'b', 'y', 'w', 'w', 'w', 'r', 'y'],
- ['y', 'g', 'b', 'g', 'o', 'w', 'y', 'b', 'g'],
- ['r', 'o', 'r', 'r', 'b', 'o', 'o', 'r', 'r'],
- ['w', 'o', 'g', 'y', 'y', 'g', 'g', 'g', 'y']]
+cube = [['r', 'o', 'w', 'b', 'g', 'g', 'y', 'r', 'w'],
+ ['g', 'b', 'o', 'w', 'r', 'r', 'g', 'g', 'r'],
+ ['g', 'b', 'b', 'w', 'w', 'r', 'g', 'y', 'r'],
+ ['y', 'r', 'y', 'w', 'o', 'y', 'r', 'b', 'o'],
+ ['w', 'o', 'o', 'g', 'b', 'o', 'w', 'o', 'y'],
+ ['b', 'y', 'o', 'w', 'y', 'y', 'b', 'g', 'b']]
+
+cube1 = [['g','g','g','g','g','g','g','g','g'], ['r','r','r','r','r','r','r','r','r'], ['w','w','w','w','w','w','w','w','w'], ['o','o','o','o','o','o','o','o','o'], ['b','b','b','b','b','b','b','b','b'], ['y','y','y','y','y','y','y','y','y']]
 
 
 #cube[r][c]
@@ -18,7 +20,9 @@ edges = [(cube[0][1], cube[2][7]), (cube[0][5], cube[1][3]), (cube[0][7], cube[5
             cube[3][5]), (cube[4][1], cube[2][1]), (cube[4][3], cube[1][5]), (cube[4][7], cube[5][7]), (cube[4][5],
             cube[3][3]), (cube[2][5], cube[1][1]), (cube[5][5], cube[1][1]), (cube[5][3], cube[3][7]), (cube[2][3], cube[3][1])]
 
-
+solved_edges = [(cube[0][1], cube[2][7]), (cube[0][5], cube[1][3]), (cube[0][7], cube[5][1]), (cube[0][4],
+            cube[3][5]), (cube[4][1], cube[2][1]), (cube[4][3], cube[1][5]), (cube[4][7], cube[5][7]), (cube[4][5],
+            cube[3][3]), (cube[2][5], cube[1][1]), (cube[5][5], cube[1][1]), (cube[5][3], cube[3][7]), (cube[2][3], cube[3][1])]
 
 def swapper(buffer):
 # Swapper function takes every case for what buffer piece is and returns setup moves for that edge, tperm, and the reverse of the setup moves
@@ -38,56 +42,76 @@ def swapper(buffer):
         newbuffer = edges[1]
         return(notation, newbuffer)
     elif buffer == ('r', 'g'):
-        notation = "d, l'" + tp + "l, d'"
+        notation = "D', l'" + tp + "l, D"
         newbuffer = (edges[1][1], edges[1][0])
         return(notation, newbuffer)
     #Green and yellow edge
     elif buffer == ('g', 'y'):
-        return("L, d', l2" + tp + "l2, d, L'")
+        notation = "L, d', l2" + tp + "l2, d, L'"
+        newbuffer = edges[2]
     elif buffer == ('y', 'g'):
-        return("d, l2" + tp + "l2, d'")
+        notation = "d, l2" + tp + "l2, d'"
+        newbuffer = (edges[2][1], edges[2][0])
     #Green and orange edge
     elif buffer == ('g', 'o'):
-        return("l'" + tp + "l")
+        notation = "l'" + tp + "l"
+        newbuffer = edges[3]
     elif buffer == ('o', 'g'):
-        return("D', l" + tp + "l', D")
+        notation = "D', l" + tp + "l', D"
+        newbuffer = (edges[3][1], edges[3][0])
     #Blue and white edge
     elif buffer == ('b', 'w'):
-        return("L', d, l2" + tp + "l2, d', L")
+        notation = "L', d, l2" + tp + "l2, d', L"
+        newbuffer = edges[4]
     elif buffer == ('w', 'b'):
-        return("L2, d', l2" + tp + "l2, d, L2")
+        notation = "L2, d', l2" + tp + "l2, d, L2"
+        newbuffer = (edges[4][1], edges[4][0])
     #Blue and red edge
     elif buffer == ('b', 'r'):
-        return("D2, l'" + tp + "l, D2")
+        notation = "D2, l'" + tp + "l, D2"
+        newbuffer = edges[5]
     elif buffer == ('r', 'b'):
-        return("D, l" + tp + "l', D'")
+        notation = "D, l" + tp + "l', D'"
+        newbuffer = (edges[5][1], edges[5][0])
     #Blue and yellow edge
     elif buffer == ('b', 'y'):
-        return("L', d', l2" + tp + "l2, d, L")
+        notation = "L', d', l2" + tp + "l2, d, L"
+        newbuffer = edges[6]
     elif buffer == ('y', 'b'):
-        return("d, l2" + tp + "l2, d'")
+        notation = "d, l2" + tp + "l2, d'"
+        newbuffer = (edges[6][1], edges[6][0])
     #Blue and orange edge
     elif buffer == ('b', 'o'):
-        return("l" + tp + "l'")
+        notation = "l" + tp + "l'"
+        newbuffer = edges[7]
     elif buffer == ('o', 'b'):
-        return("D, l'" + tp + "l, D'")
+        notation = "D, l'" + tp + "l, D'"
+        newbuffer = (edges[7][1], edges[7][0])
     #White and orange edge
     elif buffer == ('w', 'o'):
-        return(tp)
+        notation = tp
+        newbuffer = edges[8]
     elif buffer == ('o', 'w'):
-        return("l, D', l" + tp + "l', D, l'")
+        notation = "l, D', l" + tp + "l', D, l'"
+        newbuffer = (edges[8][1], edges[8][0])
     #Orange and yellow edge
     elif buffer == ('o', 'y'):
-        return("l', D', l" + tp + "l', D, l")
+        notation = "l', D', l" + tp + "l', D, l"
+        newbuffer = edges[9]
     elif buffer == ('y', 'o'):
-        return("l2" + tp + "l2")
+        notation = "l2" + tp + "l2"
+        newbuffer = (edges[9][1], edges[9][0])
     #Yellow and red edge
     elif buffer == ('y', 'r'):
-        return("d2, l2" + tp + "l2, d2")
+        notation = "d2, l2" + tp + "l2, d2"
+        newbuffer = edges[10]
     elif buffer == ('r', 'y'):
-        return("d', L, d, l2" + tp + "l2, d', L', d")
+        notation = "d', L, d, l2" + tp + "l2, d', L', d"
+        newbuffer = (edges[10][1], edges[10][0])
+    return(notation, newbuffer)
 
 def solver(edges2):
+    while edges !=
     notation, newbuffer = swapper(edges[8])
     print(notation)
     notation, newbuffer = swapper(newbuffer)
